614e5d6193cd6483f5496a8a1504aedbef4a4f27
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-07-10 22:41:20 +0200

    Added register form that inserts data to "users" collection in db

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import bcrypt
 
 if os.path.exists("env.py"):
   import env 
 
 app = Flask(__name__)
-
+app.secret_key = os.environ["SECRET_KEY"]
 app.config["MONGO_DBNAME"] = os.environ["MONGO_DBNAME"]
 app.config["MONGO_URI"] = os.environ["MONGO_URI"]
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-  return render_template("index.html", users = mongo.db.users.find({"first_name" : "Mark"}))
+    if "email" in session:
+      userName = session["email"] 
+      return render_template("index.html")
+    return render_template("login.html")
+      
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+      users = mongo.db.users
+      emailExist = users.find_one({"email" : request.form["email"]})
+
+      if emailExist is None:
+        password_hashed = bcrypt.hashpw(request.form["password"].encode("utf-8"), bcrypt.gensalt())
+        users.insert({  'first_name' : request.form["first-name"], 
+                        'last_name' : request.form["last-name"], 
+                        'email' : request.form["email"], 
+                        'password' : password_hashed
+                    })
+        session['email'] = request.form["email"]
+        return redirect(url_for("index"))
+      
+      return "already!!!"
+    
+    return render_template('signup.html')
+
+
+
 
 
 if __name__ == '__main__':
