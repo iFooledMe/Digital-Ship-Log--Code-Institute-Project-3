f6e12c4aefb1363e5dce97b5ffa6224a20e44d64
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-07-10 11:29:06 +0200

    Added connection to MongoDB Atlas + set up a env.py file to run locally + added this env.py file in a new .gitignore

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+if os.path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ["MONGO_DBNAME"]
+app.config["MONGO_URI"] = os.environ["MONGO_URI"]
+mongo = PyMongo(app)
+
 @app.route('/')
-def testFunction():
-    return 'some text'
+def index():
+  return render_template("index.html", users = mongo.db.users.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
