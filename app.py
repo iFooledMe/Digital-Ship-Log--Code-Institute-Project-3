781a8e71f328de83da9d4f011d053f483491ae60
iFooledMe <60869593+iFooledMe@users.noreply.github.com>
2020-07-11 14:23:46 +0200

    Added Logout link (clear session)+ display name for user in session (after creating an account)

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ app.config["MONGO_DBNAME"] = os.environ["MONGO_DBNAME"]
 app.config["MONGO_URI"] = os.environ["MONGO_URI"]
 mongo = PyMongo(app)
 
+# ==== INDEX ====================================================================================================================================== 
 @app.route('/')
 def index():
-    if "email" in session:
-      userName = session["email"] 
-      return render_template("index.html")
+    if "userName" in session:
+      return render_template("index.html", users = mongo.db.users.find({"email" : session['userName']}))
     return render_template("login.html")
-      
+
+# ==== SIGNUP FORM AND DB INSERT (if not in session and user already exists) =======================================================================
+# TODO: Fix issue with login users accessing this route    
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
@@ -33,17 +35,23 @@ def signup():
                         'email' : request.form["email"], 
                         'password' : password_hashed
                     })
-        session['email'] = request.form["email"]
+        session['userName'] = request.form["email"]
         return redirect(url_for("index"))
       
       return "already!!!"
-    
     return render_template('signup.html')
 
+# ==== LOG OUT (Clear session vars) ================================================================================================================
+@app.route("/logout")
+def logout():
+	session.clear()
+	return redirect(url_for('index'))
+
+	
 
 
 
-
+# ==== APP.RUN =====================================================================================================================================
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=os.environ.get('PORT'),
